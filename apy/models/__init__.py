3ef0d93c5a2cdc71610354f46cecc530322dc6c1
Volkan Gurel <vol.gurel@gmail.com>
2012-10-22 08:01:53 -0400

    removed star imports in favor of explicit imports in forms and models

--- apy/models/__init__.py
+++ apy/models/__init__.py
@@ -1,2 +1,2 @@
-from .fields import *
-from .models import *
+from .fields import BaseField, IntegerField, LongField, StringField, ArrayField, ObjectField, TimeField, DateTimeField, LinkField, ValidationError
+from .models import ApiModelMetaClass, ApiModelPermissions, BaseApiModel
