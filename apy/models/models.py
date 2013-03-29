51d596b5101f9015a4b8651a52648f2f7b0f4f5c
Volkan Gurel <vol.gurel@gmail.com>
2013-03-29 00:41:13 -0400

    started using setuptools for builds

--- apy/models/models.py
+++ apy/models/models.py
@@ -1,9 +1,9 @@
 import collections
 import re
 
-from apy.models import fields as apy_fields
 from django import forms
 
+from apy.models import fields as apy_fields
 
 MODELS = {}
 
--- setup.py
+++ setup.py
@@ -1,40 +1,16 @@
-import os
-from distutils.core import setup
+#!/usr/bin/env python
+from setuptools import setup, find_packages
 
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
+setup(
+    name='apy',
+    version='0.1.0',
 
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-apy_dir = 'apy'
+    author='Volkan Gurel',
+    author_email='me@volkangurel.com',
+    url='https://github.com/volkangurel/arzlan',
 
-for dirpath, dirnames, filenames in os.walk(apy_dir):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
-
-setup(name='apy',
-      version='0.1.0',
-      packages=packages,
-      data_files=data_files,
-      )
+    packages=find_packages(),
+    install_requires=[
+        'django >= 1.5.1',
+        ],
+    )
